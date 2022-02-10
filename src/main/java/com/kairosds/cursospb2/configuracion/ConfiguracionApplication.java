630e8dfb50c01e8715f973b36ff455850df7090f
Sergio Gumiel <sergio.gumiel@kairosds.com>
2022-02-10 20:08:53 +0100

    First commit

---
+++ src/main/java/com/kairosds/cursospb2/configuracion/ConfiguracionApplication.java
@@ -0,0 +1,13 @@
+package com.kairosds.cursospb2.configuracion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfiguracionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfiguracionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kairosds/cursospb2/configuracion/ConfiguracionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kairosds.cursospb2.configuracion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfiguracionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
