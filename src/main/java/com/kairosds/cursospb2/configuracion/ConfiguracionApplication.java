aafaa64df40638efec34be58bf15195691f5b35e
Sergio Gumiel <sergio.gumiel@kairosds.com>
2022-02-10 20:18:50 +0100

    Configuracion con properties

--- src/main/java/com/kairosds/cursospb2/configuracion/ConfiguracionApplication.java
+++ src/main/java/com/kairosds/cursospb2/configuracion/ConfiguracionApplication.java
@@ -1,13 +1,24 @@
 package com.kairosds.cursospb2.configuracion;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class ConfiguracionApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConfiguracionApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner execute(Environment environment){
+		return args -> {
+			System.out.println(environment.getProperty("saludo.corto"));
+			System.out.println(environment.getProperty("saludo.largo"));
+		};
 	}
 
 }
